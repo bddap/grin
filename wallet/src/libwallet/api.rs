e42b0f717ee36f9b4468f5190cd008f1fa957f5e
Andrew Dirksen <andrew@dirksen.com>
2019-01-17 19:10:46 -0800

    bak

--- wallet/src/libwallet/api.rs
+++ wallet/src/libwallet/api.rs
@@ -905,3 +905,315 @@ where
 		}
 	}
 }
+
+/// This module implements Into<jsonrpc_core::IoHandler> for APIForeign and APIOwner
+/// APIOwner implementation in progress
+mod jsonrpc_api {
+	use crate::keychain::Keychain;
+	use crate::libwallet::api::APIForeign;
+	use crate::libwallet::types::{BlockFees, NodeClient, WalletBackend};
+	use jsonrpc_core::{Error, IoHandler, Params, Value};
+
+	// We need to condider how this api should report errors. There are several
+	// options.
+	//
+	// 1. All procedures return a Result which is serialzed using serde. This could
+	//    be a security concern, as it may leak sensitive data to api clients.
+	// 2. All procedures return a Result which is not serialized; instead it is
+	//    reported as an opaque "Internal Error".
+	// 3. The jsonrpc 2.0 spec provides a mechanism for reporting internal errors
+	//    https://www.jsonrpc.org/specification#error_object
+	//    Use of this mechanism has pros and cons:
+	//    pros:
+	//        - Conforms to jsonrpc user expectations
+	//        - More easily consumable by non-rust clients
+	//    cons:
+	//        - Each error type must have an associated number id, assigning ids
+	//          will likely be a manual process.
+	//    Jsonrpc errors MAY include a structured "data" field. Internal errors
+	//    would be serialized into the field using serde. As with option 1, detailed
+	//    error messages could leak sensitive information.
+	// 4. Use jsonrpc error reporting, but report iternal errors as an opaque
+	//    "Internal Error".
+	//
+	// The options, in order of ease of implementation are: 2, 4, 1, 3.
+	//
+	// Options 1 and 3, the options involving structured error reporting, are slightly more
+	// difficult to implement because grin errors contain failure::Context objects.
+	// AFIK failure::Context does not implement serde Serialize and Deserialze traits.
+	//
+	// Baring further disscussion. Option 2 will be used, as it is simplest to implement and
+	// does not potentially leak sensitive information through structured errors.
+
+	impl<W, C, K> Into<IoHandler> for APIForeign<W, C, K>
+	where
+		W: ?Sized + WalletBackend<C, K> + 'static,
+		C: NodeClient + 'static,
+		K: Keychain + 'static,
+	{
+		fn into(self) -> IoHandler {
+			let mut io = IoHandler::new(); // Value to be returned.
+
+			// each closure gets its own copy of the API object; copies are cheap
+			let api_copy = self.clone();
+			add_rpc_method(
+				&mut io,
+				"build_coinbase",
+				&["block_fees"],
+				move |mut args: Vec<Value>| {
+					let expected = 1;
+					let actual = args.len();
+					debug_assert_eq!(expected, actual); // ensured by get_rpc_args
+					let unreachable_argnum_err =
+						|| InvalidArgs::WrongNumberOfArgs { expected, actual };
+					let mut ordered_args = args.drain(..);
+
+					// parse each arguments in order
+					let next_arg = ordered_args.next().ok_or_else(unreachable_argnum_err)?;
+					let arg0: BlockFees = serde_json::from_value(next_arg).map_err(|_| {
+						InvalidArgs::InvalidArgStructure {
+							name: "block_fees",
+							index: 0,
+						}
+					})?;
+
+					// Api object will be mutated, we make a copy so rustc will let us call mutable
+					// methods.
+					let mut api = api_copy.clone();
+
+					// call the target procedure
+					let res = api.build_coinbase(&arg0).map_err(|_| InternalError);
+
+					// serialize result into a json value
+					let ret = serde_json::to_value(res).expect(
+						"serde_json::to_value unexpectedly returned an error, this shouldn't have \
+						 happened because serde_json::to_value does not perform io.",
+					);
+
+					Ok(ret)
+				},
+			);
+
+			let api_copy = self.clone();
+			add_rpc_method(
+				&mut io,
+				"verify_slate_messages",
+				&["slate"],
+				move |mut args: Vec<Value>| {
+					let expected = 1;
+					let actual = args.len();
+					debug_assert_eq!(expected, actual); // ensured by get_rpc_args
+					let unreachable_argnum_err =
+						|| InvalidArgs::WrongNumberOfArgs { expected, actual };
+					let mut ordered_args = args.drain(..);
+
+					// parse each arguments in order
+					let next_arg = ordered_args.next().ok_or_else(unreachable_argnum_err)?;
+					let arg0: grin_core::libtx::slate::Slate = serde_json::from_value(next_arg)
+						.map_err(|_| InvalidArgs::InvalidArgStructure {
+							name: "slate",
+							index: 0,
+						})?;
+
+					// Api object will be mutated, we make a copy so rustc will let us call mutable
+					// methods.
+					let mut api = api_copy.clone();
+
+					// call the target procedure
+					let res = api.verify_slate_messages(&arg0).map_err(|_| InternalError);
+
+					// serialize result into a json value
+					let ret = serde_json::to_value(res).expect(
+						"serde_json::to_value unexpectedly returned an error, this shouldn't have \
+						 happened because serde_json::to_value does not perform io.",
+					);
+
+					Ok(ret)
+				},
+			);
+
+			let api_copy = self.clone();
+			add_rpc_method(
+				&mut io,
+				"receive_tx",
+				&["slate", "dest_acct_name", "message"],
+				move |mut args: Vec<Value>| {
+					let expected = 3;
+					let actual = args.len();
+					debug_assert_eq!(expected, actual); // ensured by get_rpc_args
+					let unreachable_argnum_err =
+						|| InvalidArgs::WrongNumberOfArgs { expected, actual };
+					let mut ordered_args = args.drain(..);
+
+					// parse each arguments in order
+					let next_arg = ordered_args.next().ok_or_else(unreachable_argnum_err)?;
+					let arg0: grin_core::libtx::slate::Slate = serde_json::from_value(next_arg)
+						.map_err(|_| InvalidArgs::InvalidArgStructure {
+							name: "slate",
+							index: 0,
+						})?;
+					let next_arg = ordered_args.next().ok_or_else(unreachable_argnum_err)?;
+					let arg1: Option<String> = serde_json::from_value(next_arg).map_err(|_| {
+						InvalidArgs::InvalidArgStructure {
+							name: "dest_acc_name",
+							index: 1,
+						}
+					})?;
+					let next_arg = ordered_args.next().ok_or_else(unreachable_argnum_err)?;
+					let arg2: Option<String> = serde_json::from_value(next_arg).map_err(|_| {
+						InvalidArgs::InvalidArgStructure {
+							name: "message",
+							index: 2,
+						}
+					})?;
+
+					// Api object will be mutated, we make a copy so rustc will let us call mutable
+					// methods.
+					let mut api = api_copy.clone();
+
+					// These conversions are necessary because receive_tx takes a mix of borrowed
+					// and owned parameters. Later on, in order to automatially generate json rpc
+					// apis arguments ownership will likely need to be homogeonus for all
+					// procedures.
+					let mut arg0_converted = arg0;
+					let arg1_converted = arg1.as_ref().map(|x| &**x);
+
+					// call the target procedure
+					let res = api
+						.receive_tx(&mut arg0_converted, arg1_converted, arg2)
+						.map_err(|_| InternalError);
+
+					// serialize result into a json value
+					let ret = serde_json::to_value(res).expect(
+						"serde_json::to_value unexpectedly returned an error, this shouldn't have \
+						 happened because serde_json::to_value does not perform io.",
+					);
+
+					Ok(ret)
+				},
+			);
+			io
+		}
+	}
+
+	fn add_rpc_method<F>(
+		iohandler: &mut IoHandler,
+		name: &'static str,
+		arg_names: &'static [&'static str],
+		cb: F,
+	) where
+		F: Fn(Vec<Value>) -> Result<Value, InvalidArgs>
+			+ std::marker::Sync
+			+ std::marker::Send
+			+ 'static,
+	{
+		iohandler.add_method(name, move |params: Params| {
+			get_rpc_args(arg_names, params)
+				.and_then(|args| cb(args))
+				.map_err(std::convert::Into::into)
+		})
+	}
+
+	// Verify and convert jsonrpc Params into owned argument list.
+	// Verifies:
+	//    - Number of args in positional parameter list is correct
+	//    - No missing args in named parameter object
+	//    - No extra args in named parameter object
+	// Absent parameter objects are interpreted as empty positional parameter lists
+	fn get_rpc_args(names: &[&'static str], params: Params) -> Result<Vec<Value>, InvalidArgs> {
+		let ar: Vec<Value> = match params {
+			Params::Array(ar) => ar,
+			Params::Map(mut ma) => {
+				let mut ar: Vec<Value> = Vec::with_capacity(names.len());
+				for name in names.iter() {
+					ar.push(
+						ma.remove(*name)
+							.ok_or(InvalidArgs::MissingNamedParameter { name })?,
+					);
+				}
+				debug_assert_eq!(ar.len(), names.len());
+				match ma.keys().next() {
+					Some(key) => return Err(InvalidArgs::ExtraNamedParameter { name: key.clone() }),
+					None => ar,
+				}
+			}
+			Params::None => vec![],
+		};
+		if ar.len() != names.len() {
+			Err(InvalidArgs::WrongNumberOfArgs {
+				expected: ar.len(),
+				actual: names.len(),
+			})
+		} else {
+			Ok(ar)
+		}
+	}
+
+	#[derive(Serialize, Deserialize)]
+	struct InternalError;
+
+	enum InvalidArgs {
+		WrongNumberOfArgs { expected: usize, actual: usize },
+		ExtraNamedParameter { name: String },
+		MissingNamedParameter { name: &'static str },
+		InvalidArgStructure { name: &'static str, index: usize },
+	}
+
+	impl Into<Error> for InvalidArgs {
+		fn into(self) -> Error {
+			match self {
+				InvalidArgs::WrongNumberOfArgs { expected, actual } => {
+					Error::invalid_params(format!(
+						"WrongNumberOfArgs. Expected {}. Actual {}",
+						expected, actual
+					))
+				}
+				InvalidArgs::ExtraNamedParameter { name } => {
+					Error::invalid_params(format!("ExtraNamedParameter {}", name))
+				}
+				InvalidArgs::MissingNamedParameter { name } => {
+					Error::invalid_params(format!("MissingNamedParameter {}", name))
+				}
+				InvalidArgs::InvalidArgStructure { name, index } => Error::invalid_params(format!(
+					"InvalidArgStructure {} at position {}.",
+					name, index
+				)),
+			}
+		}
+	}
+
+	#[cfg(test)]
+	mod test {
+		use crate::keychain::ExtKeychain;
+		use crate::libwallet::api::{APIForeign, APIOwner};
+		use crate::libwallet::types::WalletBackend;
+		use crate::util;
+		use crate::{HTTPNodeClient, LMDBBackend, WalletConfig};
+		use jsonrpc_core::IoHandler;
+		use std::sync::Arc;
+
+		fn create_api_both() -> (
+			APIOwner<WalletBackend<HTTPNodeClient, ExtKeychain>, HTTPNodeClient, ExtKeychain>,
+			APIForeign<WalletBackend<HTTPNodeClient, ExtKeychain>, HTTPNodeClient, ExtKeychain>,
+		) {
+			let wallet_config = WalletConfig::default();
+			let node_client = HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
+			let wallet: Arc<util::Mutex<WalletBackend<HTTPNodeClient, ExtKeychain>>> = Arc::new(
+				util::Mutex::new(LMDBBackend::new(wallet_config.clone(), "", node_client).unwrap()),
+			);
+			(
+				APIOwner::new(wallet.clone()),
+				*APIForeign::new(wallet.clone()),
+			)
+		}
+
+		fn create_api_foreign() -> IoHandler {
+			create_api_both().1.into()
+		}
+
+		#[test]
+		fn api_foreign_create_keybase() {
+			let api = create_api_foreign();
+		}
+	}
+}
